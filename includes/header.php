225a2583148541187a763b4b08335c2e66005850
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-27 19:47:50 +0300

    wrap brand text in span for mobile hide

--- includes/header.php
+++ includes/header.php
@@ -62,7 +62,7 @@ $baseUrl = $isSubDir ? '../' : '';
         <!-- App brand -->
         <a href="<?= $baseUrl ?>index.php" class="brand">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg>
-            ICT Support
+            <span class="brand-text">ICT Support</span>
         </a>
 
         <!-- Mobile hamburger button -->
